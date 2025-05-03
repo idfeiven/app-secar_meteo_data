fee2a3e46ce7682ab60c961da5c622c09d97bd7b
ivan.dominguez <ivandominguezfuentes64@gmail.com>
2025-05-03 21:23:38 +0200

    [UPD] Refactored monthly data processing and updated column names for clarity

--- app/modules/mean_values_page.py
+++ app/modules/mean_values_page.py
@@ -6,38 +6,13 @@ from common import load_daily_data, get_monthly_data, plot_interactive_data_by_y
 
 # -----------------------------------FUNCTIONS-----------------------------------
 
-def get_monthly_data_for_plots(daily_data):
-    #resample daily data to monthly data:
-    max_monthly_data = daily_data.resample('ME').max()\
-                        [['high_temp_deg', 'wind_gust_kmh', 'rain_10min_mm', 'rain_rate_mmh']]\
-                        .rename(columns = {'high_temp_deg': 'max_high_temp_deg',
-                                           'wind_gust_kmh': 'max_wind_gust_kmh',
-                                           'rain_10min_mm': 'max_rain_10min_mm',
-                                           'rain_rate_mmh': 'max_rain_rate_mmh'})
-
-    min_monthly_data = daily_data.resample('ME').min()[['low_temp_deg']]\
-        .rename(columns= {'low_temp_deg': 'min_low_temp_deg'})
-
-    mean_monthly_data = daily_data\
-        .resample('ME').mean()[['temp_out_deg', 'high_temp_deg', 'low_temp_deg' ,'mean_rel_humidity_perc', 'dewpoint_deg', 'wind_speed_kmh', 'mean_pressure_hPa']]\
-        .rename(columns={'temp_out_deg': 'mean_temp_out_deg',
-                         'high_temp_deg': 'mean_high_temp_deg',
-                         'low_temp_deg': 'mean_low_temp_deg',
-                         'dewpoint_deg': 'mean_dewpoint_deg',
-                         'wind_speed_kmh': 'mean_wind_speed_kmh',
-                         'mean_pressure_hPa': 'mean_pressure_hPa'})
-
-    monthly_pcp_data = daily_data.resample('ME').sum()[['pcp (mm)']]
-    #group all monthly data
-    monthly_data = pd.concat([max_monthly_data, min_monthly_data, mean_monthly_data, monthly_pcp_data], axis = 1).rename(columns = {'pcp (mm)': 'pcp_acum_month_mm'})
-    monthly_data = monthly_data.reset_index()  
-    return(monthly_data) 
-
-
 def get_monthly_data_means(monthly_data):
     monthly_data_means = monthly_data.groupby(monthly_data['date'].dt.strftime('%m')).mean()\
-        .drop(['max_high_temp_deg', 'max_wind_gust_kmh', 'max_rain_10min_mm', 'max_rain_rate_mmh',
-                'min_low_temp_deg'], axis = 1).rename(columns = {'pcp_acum_month_mm': 'mean_pcp_acum_month_mm'})
+        .drop(['Monthly max. temperature (°C)',
+               'Monthly max. wind gust (km/h)',
+               'Monthly max. rain in 10 minutes (mm)',
+               'Monthly max. instantaneous rain rate (mm/h)',
+               'Monthly min. temperature (°C)'], axis = 1).rename(columns = {'Monthly precipitation (mm)': 'Mean monthly precipitation (mm)'})
     
     return(monthly_data_means)
 
@@ -52,17 +27,17 @@ monthly_data_means.set_index('date', inplace = True)
 
 
 st.markdown("## Temperatura mensual")
-fig = plot_interactive_data_by_year(monthly_data, 'mean_high_temp_deg', "Temperatura máxima media mensual", "T (°C)")
+fig = plot_interactive_data_by_year(monthly_data, 'Monthly mean max. temperatures (°C)', "Temperatura máxima media mensual", "T (°C)")
 st.plotly_chart(fig, use_container_width=True)
 
-fig = plot_interactive_data_by_year(monthly_data, 'mean_low_temp_deg', "Temperatura mínima media mensual", "T (°C)")
+fig = plot_interactive_data_by_year(monthly_data, 'Monthly mean min. temperatures (°C)', "Temperatura mínima media mensual", "T (°C)")
 st.plotly_chart(fig, use_container_width=True)
 
-fig = plot_interactive_data_by_year(monthly_data, 'mean_temp_out_deg', "Temperatura media mensual", "T (°C)")
+fig = plot_interactive_data_by_year(monthly_data, 'Monthly mean temperature (°C)', "Temperatura media mensual", "T (°C)")
 st.plotly_chart(fig, use_container_width=True)
 
 st.markdown("## Precipitación mensual y anual")
-fig = plot_interactive_data_by_year(monthly_data, 'pcp_acum_month_mm', "Precipitación acumulada mensual", "mm")
+fig = plot_interactive_data_by_year(monthly_data, 'Monthly precipitation (mm)', "Precipitación acumulada mensual", "mm")
 st.plotly_chart(fig, use_container_width=True)
 
 st.markdown("## Valores medios mensuales en tabla")
@@ -74,12 +49,15 @@ monthly_data_means["Month"] = np.arange(1,13)
 monthly_data_means.set_index("Month", inplace=True)
 monthly_data_means = monthly_data_means.round(2)
 
-monthly_data_means.rename(columns = {"mean_temp_out_deg": "Temperatura Media (°C)",
-                                        "mean_high_temp_deg": "Media de Temperaturas Máximas (°C)",
-                                        "mean_low_temp_deg": "Media de Temperaturas Mínimas (°C)",
-                                        "mean_rel_humidity_perc": "Humedad Relativa Media (%)",
-                                        "mean_dewpoint_deg": "Temperatura Media de Punto de Rocío (°C)",
-                                        "mean_wind_speed_kmh": "Velocidad Media del Viento (km/h)",
-                                        "mean_pressure_hPa": "Presión Media al Nivel del Mar (hPa)",
-                                        "mean_pcp_acum_month_mm": "Precipitación Media (mm)"}, inplace=True)
+
+k_trans = ["Temperatura Media (°C)",
+           "Media de Temperaturas Máximas (°C)",
+           "Media de Temperaturas Mínimas (°C)",
+           "Humedad Relativa Media (%)",
+           "Temperatura Media de Punto de Rocío (°C)",
+           "Velocidad Media del Viento (km/h)",
+           "Presión Media al Nivel del Mar (hPa)",
+           "Precipitación Media (mm)"]
+
+monthly_data_means.rename(columns = dict(zip(monthly_data_means.columns, k_trans)), inplace=True)
 st.dataframe(monthly_data_means)
--- app/modules/monthly_summary_page.py
+++ app/modules/monthly_summary_page.py
@@ -7,27 +7,26 @@ daily_data = load_daily_data()
 monthly_data = get_monthly_data(daily_data)
 
 st.markdown("## Resúmenes mensuales de datos")
-monthly_data = monthly_data.copy()
-monthly_data['date'] = monthly_data.index
-monthly_data = monthly_data.reset_index(drop=True)
 monthly_data.sort_values(by = "date", ascending = False, inplace=True)
 monthly_data["date"]= monthly_data["date"].dt.strftime("%Y-%m")
 monthly_data = monthly_data.round(2)
 
-monthly_data.rename(columns = {'date': "Año y mes",
-                               'max_high_temp_deg': 'Temperatura máxima mensual (°C)',
-                               'max_wind_gust_kmh': 'Ráfaga máxima de viento mensual (km/h)',
-                               'max_rain_10min_mm': 'Máxima lluvia en 10 minutos mensual (mm)',
-                               'max_rain_rate_mmh': 'Máxima tasa de lluvia instantánea mensual (mm/h)',
-                               'mean_temp_out_deg': 'Temperatura media mensual (°C)',
-                               'min_low_temp_deg': "Temperatura mínima mensual (°C)",
-                               'pcp_acum_month_mm': "Precipitación mensual acumulada (mm)",
-                               'mean_high_temp_deg': 'Temperatura máxima media mensual (°C)',
-                               'mean_low_temp_deg': 'Temperatura mínima media mensual (°C)',
-                               'mean_rel_humidity_perc': 'Humedad relativa media mensual (%)',
-                               'mean_dewpoint_deg': 'Punto de rocío medio mensual (°C)',
-                               'mean_wind_speed_kmh': 'Velocidad media del viento mensual (km/h)',
-                               'mean_pressure_hPa': 'Presión media mensual (hPa)'}, inplace = True)
-monthly_data.set_index("Year and month", inplace = True)
+k_trans = ["Año y mes",
+           'Temperatura máxima mensual (°C)',
+           'Ráfaga máxima de viento mensual (km/h)',
+           'Máxima lluvia en 10 minutos mensual (mm)',
+           'Máxima tasa de lluvia instantánea mensual (mm/h)',
+           'Temperatura media mensual (°C)',
+           "Temperatura mínima mensual (°C)",
+           "Precipitación mensual acumulada (mm)",
+           'Temperatura máxima media mensual (°C)',
+           'Temperatura mínima media mensual (°C)',
+           'Humedad relativa media mensual (%)',
+           'Punto de rocío medio mensual (°C)',
+           'Velocidad media del viento mensual (km/h)',
+           'Presión media mensual (hPa)']
+
+monthly_data.rename(columns = dict(zip(monthly_data.columns, k_trans)), inplace = True)
+monthly_data.set_index("Año y mes", inplace = True)
 
 st.dataframe(monthly_data)
