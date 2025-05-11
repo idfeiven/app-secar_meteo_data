4710adccf664402bdeaaa5ba791233121788345c
ivan.dominguez <ivandominguezfuentes64@gmail.com>
2025-05-11 10:44:10 +0200

    [UPD] Updated monthly data processing and visualization labels to Spanish for improved localization

--- app/modules/mean_values_page.py
+++ app/modules/mean_values_page.py
@@ -8,11 +8,11 @@ from common import load_daily_data, get_monthly_data, plot_interactive_data_by_y
 
 def get_monthly_data_means(monthly_data):
     monthly_data_means = monthly_data.groupby(monthly_data['date'].dt.strftime('%m')).mean()\
-        .drop(['Monthly max. temperature (°C)',
-               'Monthly max. wind gust (km/h)',
-               'Monthly max. rain in 10 minutes (mm)',
-               'Monthly max. instantaneous rain rate (mm/h)',
-               'Monthly min. temperature (°C)'], axis = 1).rename(columns = {'Monthly precipitation (mm)': 'Mean monthly precipitation (mm)'})
+        .drop(['Temperatura máxima mensual (°C)',
+               'Racha de viento máxima mensual (km/h)',
+               'Precipitación máxima 10-minutal (mm)',
+               'Máxima intensidad de lluvia (mm/h)',
+               'Temperatura mínima mensual (°C)'], axis = 1)
     
     return(monthly_data_means)
 
@@ -30,17 +30,17 @@ monthly_data_means.set_index('date', inplace = True)
 
 
 st.markdown("## Temperatura mensual")
-fig = plot_interactive_data_by_year(monthly_data, 'Monthly mean max. temperatures (°C)', "Temperatura máxima media mensual", "T (°C)")
+fig = plot_interactive_data_by_year(monthly_data, 'Media mensual de temperaturas máximas (°C)', "Temperatura máxima media mensual", "T (°C)")
 st.plotly_chart(fig, use_container_width=True)
 
-fig = plot_interactive_data_by_year(monthly_data, 'Monthly mean min. temperatures (°C)', "Temperatura mínima media mensual", "T (°C)")
+fig = plot_interactive_data_by_year(monthly_data, 'Media mensual de temperaturas mínimas (°C)', "Temperatura mínima media mensual", "T (°C)")
 st.plotly_chart(fig, use_container_width=True)
 
-fig = plot_interactive_data_by_year(monthly_data, 'Monthly mean temperature (°C)', "Temperatura media mensual", "T (°C)")
+fig = plot_interactive_data_by_year(monthly_data, 'Temperatura media mensual (°C)', "Temperatura media mensual", "T (°C)")
 st.plotly_chart(fig, use_container_width=True)
 
 st.markdown("## Precipitación mensual y anual")
-fig = plot_interactive_data_by_year(monthly_data, 'Monthly precipitation (mm)', "Precipitación acumulada mensual", "mm")
+fig = plot_interactive_data_by_year(monthly_data, 'Precipitación mensual (mm)', "Precipitación acumulada mensual", "mm")
 st.plotly_chart(fig, use_container_width=True)
 
 st.markdown("## Valores medios mensuales en tabla")
